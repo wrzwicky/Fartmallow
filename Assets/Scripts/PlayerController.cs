5b691f6c545099d5a6a5aa3cd1183e4ffc175ce3
William R. Zwicky <wrzwicky@pobox.com>
2018-09-09 13:16:19 -0700

    Player Controller and dummy level

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10;
+    public Vector3 jumpDir = new Vector3(0, 10, 0);
+
+    CharacterController myCC;
+    Rigidbody myRB;
+
+	// Use this for initialization
+	void Start () {
+        myCC = GetComponent<CharacterController>();
+        myRB = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // basic movement
+        float rt = Input.GetAxis("Horizontal");
+        float up = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(rt, 0, up);
+        //myCC.SimpleMove(move * speed);
+        myRB.AddForce(move * speed);
+
+        // jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            if(myRB.velocity.y == 0)
+                myRB.AddForce(jumpDir, ForceMode.Impulse);
+        }
+    }
+}
