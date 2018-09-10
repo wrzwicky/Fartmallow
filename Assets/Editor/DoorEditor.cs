015957d7e2997e029e37e03c589c3553191a52f1
Altered Mechanics <alteredmechanics@gmail.com>
2018-09-09 17:29:35 -0700

    Working Door

---
+++ Assets/Editor/DoorEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Door))]
+public class DoorEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        Door door = (Door)target;
+
+        if(Application.isPlaying)
+        {
+            if (door.IsOpen)
+            {
+                if (GUILayout.Button("Close"))
+                    door.Close();
+            }
+            else
+            {
+                if (GUILayout.Button("Open"))
+                    door.Open();
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    static Manager ms_instance = null;
+
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        // simple cheap singleton
+        if (ms_instance != null)
+            Debug.LogError("Only one instance of the manager is allowed");
+        ms_instance = this;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
